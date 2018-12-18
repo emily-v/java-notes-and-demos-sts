34ad44b5a73078461550b4216853862c8f5497a9
Emily Vong <emily.vong25@gmail.com>
2018-12-18 13:24:38 -0500

    almost

--- CreditCardProject/src/main/java/creditcard/CreditCard.java
+++ CreditCardProject/src/main/java/creditcard/CreditCard.java
@@ -1,6 +1,8 @@
 package creditcard;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -9,15 +11,16 @@ public class CreditCard {
 	private double balance = 0;
 	private int rewardPoints = 0;
 	double limit = 1000;
+	boolean approve;
 
 	
 	// constructor with params
-	public CreditCard(double balance, int rewardPoints, double limit) {
-		super();
-		this.balance = balance;
-		this.rewardPoints = rewardPoints;
-		this.limit = limit;
-	}
+//	public CreditCard(double balance, int rewardPoints, double limit) {
+//		super();
+//		this.balance = balance;
+//		this.rewardPoints = rewardPoints;
+//		this.limit = limit;
+//	}
 
 	// default constructor
 	public CreditCard() {
@@ -33,20 +36,59 @@ public class CreditCard {
 		this.balance = balance;
 	}
 	
-	public double makePurchase (double balanceParam, double priceParam) {
+	public boolean approvePurchase (double balanceParam, double priceParam) {
 		double attemptedBalance = balanceParam + priceParam;
 		if (attemptedBalance <= limit) {
+			approve = true;
+		} else {
+			approve = false;
+		}
+		return approve;
+	}
+	
+	public double makePurchase (double balanceParam, double priceParam) {
+		double attemptedBalance = balanceParam + priceParam;
+		if (approve == true) {
 			this.balance = attemptedBalance;
 			System.out.println("Purchase Approved\n");
 		} else {
-			System.out.println("Purchase Denied: Spending over limit\n");
+		System.out.println("Purchase Denied: Spending over limit\n");
 		}
 		return this.balance;
 	}
 	
-	public int earnPoints() {
-		this.rewardPoints = (int) (this.balance*.1);
+//	public double makePurchase (double balanceParam, double priceParam) {
+//		double attemptedBalance = balanceParam + priceParam;
+//		if (attemptedBalance <= limit) {
+//			this.balance = attemptedBalance;
+//			System.out.println("Purchase Approved\n");
+//		} else {
+//			System.out.println("Purchase Denied: Spending over limit\n");
+//		}
+//		return this.balance;
+//	}
+	
+	public int earnPoints(double purchaseAmount) {
+		if (approve == true) {
+			this.rewardPoints = this.rewardPoints + ((int) (purchaseAmount*.1));
+			
+		} 
 		return this.rewardPoints;
+	}
+	
+//	public int earnPoints() {
+//		this.rewardPoints = (int) (this.balance*.1);
+//		return this.rewardPoints;
+//	}
+	
+	public void clearRewards() {
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter("../CreditCardProject/src/main/resources/rewards.txt"));
+			writer.write("Rewards Balance History (last value is current balance)\n");
+			writer.close();
+		} catch (IOException e) {
+			System.out.println("rewards file not found");
+		}
 	}
 	
 	public void writeRewards(Integer pointBalance) {
@@ -60,9 +102,24 @@ public class CreditCard {
 		}
 	}
 	
+	public String readRewards() {
+		String tempLast = null;
+		String lastLine = null;
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("../CreditCardProject/src/main/resources/rewards.txt"));
+			while ((tempLast = reader.readLine()) != null) {
+				lastLine = tempLast;
+			}
+			reader.close();
+		} catch (IOException e) {
+			System.out.println("rewards file not found");
+		}
+		return lastLine;
+	}
+	
 	public int redeemPoints(String choice) {
+		boolean invalid = false;
 		int val = 0;
-		
 		switch (choice) {
       	case "A":
       		val = 20;
@@ -75,35 +132,18 @@ public class CreditCard {
       		break;
       	default:
       		System.out.println("Invalid choice.");
+      		invalid = true;
 	  }
-		if (val > this.rewardPoints) {
-			System.out.println("Insufficient reward points available\n");
-		} else {
-			this.rewardPoints = this.rewardPoints - val;
-			System.out.println("You've redeemed your reward!\n");
+		if (invalid == false) {
+			if (val > this.rewardPoints) {
+				System.out.println("Insufficient reward points available\n");
+			} else {
+				this.rewardPoints = this.rewardPoints - val;
+				System.out.println("You've redeemed your reward!\n"); // TODO prevent this line from printing if Invalid Choice
+			}
 		}
-		
 		return this.rewardPoints;
 	}
-	
-//	public int redeemPoints(String choice) {
-//		switch (choice) {
-//      	case "A":
-//      		this.rewardPoints = this.rewardPoints - 20;
-//      		break;
-//      	case "B":
-//      		this.rewardPoints = this.rewardPoints - 150;
-//      		break;
-//      	case "C":
-//      		this.rewardPoints = this.rewardPoints - 250;
-//      		break;
-//      	default:
-//      		System.out.println("Invalid choice.");
-//	  }
-//		
-//		return this.rewardPoints;
-//	}
-
 	
 	public int getRewardPoints() {
 		return this.rewardPoints;
--- CreditCardProject/src/main/java/creditcard/Transactions.java
+++ CreditCardProject/src/main/java/creditcard/Transactions.java
@@ -2,6 +2,7 @@ package creditcard;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
@@ -13,7 +14,7 @@ public class Transactions {
 		
 		CreditCard card = new CreditCard();
 		
-		// -- Creating Rewards as ArrayList rewardsMenu
+		// -- Creating Rewards as ArrayList rewardsMenu -- * don't need to have separate arguments *
 		Reward ride = new Reward("A","$20 Uber Credit", 20);
 		Reward hotel = new Reward("B", "$150 Hotel Credit", 150);
 		Reward airfare = new Reward("C", "$250 Airfare Credit", 250);
@@ -42,6 +43,7 @@ public class Transactions {
 		// -- start up working with any character key + ENTER
 		System.out.println("Press any character key to start the program"); // TODO implement start on Y input
         String start = input.next().toLowerCase();
+        card.clearRewards();
         boolean quit = false;	
         // -- END
         
@@ -52,37 +54,47 @@ public class Transactions {
             System.out.println("3. Redeem Rewards");
             System.out.println("0. Quit");  
         	System.out.println("Choose menu item: ");
-              menuChoice = input.nextInt(); // TODO catch InputMismatchException
-              switch (menuChoice) {
-              case 1:
-            	  System.out.println("Enter purchase amount");
-            	  double price = input.nextDouble();
-            	  card.makePurchase(card.getBalance(), price);
-            	  card.earnPoints();
-            	  card.writeRewards(card.getRewardPoints());
-                  break;
-              case 2:
-                  System.out.println("Your credit card balance is: $" + card.getBalance());
-                  System.out.println("Your reward points balance is: " + card.getRewardPoints() + " points\n");
-            	  break;
-              case 3:
-            	  for(Reward r : rewardsMenu) {
-          			r.listRewards();
-          		  }
-            	  System.out.println("Choose your reward!");
-            	  String rewardChoice = input.next().toUpperCase();
-            	  card.redeemPoints(rewardChoice);
-            	  card.writeRewards(card.getRewardPoints());
-            	  break;
-              case 0:
-            	  System.out.println("You have quit the program");
-            	  quit = true;
-            	  break;
-              default:
-                    System.out.println("Invalid choice.");
-              }
-        } while (!quit);
-        
+        	try {
+//        		if (input.hasNextInt() == true) {
+//        			menuChoice = input.nextInt();
+//        		} else {
+//        			System.out.println("invalid entry type");
+//        		}
+	        	menuChoice = input.nextInt(); // TODO catch InputMismatchException
+	    		switch (menuChoice) {
+	    			case 1:
+	    				System.out.println("Enter purchase amount");
+	    				double price = input.nextDouble();
+	    				card.approvePurchase(card.getBalance(), price);
+	    				card.makePurchase(card.getBalance(), price);
+	    				card.earnPoints(price);
+	    				card.writeRewards(card.getRewardPoints());
+	    				break;
+	    			case 2:
+	    				System.out.println("Your credit card balance is: $" + card.getBalance());
+	    				System.out.println("Your reward points balance is: " + card.readRewards() + " points\n"); // changed card.getRewardPoints() to card.readRewards()
+	    				break;
+	    			case 3:
+	    				for(Reward r : rewardsMenu) {
+	    					r.listRewards();
+	    				}
+	    				System.out.println("Choose your reward!");
+	    				System.out.println("You currently have " + card.readRewards() + " reward points to redeem.");
+	    				String rewardChoice = input.next().toUpperCase();
+	    				card.redeemPoints(rewardChoice);
+	    				card.writeRewards(card.getRewardPoints());
+	    				break;
+	    			case 0:
+	    				System.out.println("You have quit the program");
+	    				quit = true;
+	    				break;
+	    			default:
+	                System.out.println("Invalid choice. Please try again.\n");
+	         	}
+        	} catch (InputMismatchException im) {
+        		System.out.println("Invalid entry type. Please try again.\n");
+        		input.next();
+        	}
+		} while (!quit);
 	}
-
 }
